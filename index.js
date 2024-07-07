0c6b8734c70a14cfef00f3ef252c54df0e1d87fe
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-07 20:48:28 +0530

    Add function to play a single round
    Intermediate functions and variables set, to completely play 1 round.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getHumanChoice(){
     let userInput = prompt("Enter your pick (among rock, paper and scissor)").toLowerCase();
     if (userInput === ''){
@@ -22,7 +25,52 @@ function getComputerChoice(){
     }
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-console.log(humanSelection)
-console.log(computerSelection)
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function showScore(){
+    console.log(`Your score is: ${humanScore}, Computer score is: ${computerScore}`);
+}
+function showChoice(){
+    console.log(`You chose: ${humanSelection}, Computer chose: ${computerSelection}`)
+}
+
+function playRound(humanChoice,computerChoice){
+    if (humanChoice == computerChoice){
+        console.log("Its a draw..");
+        showChoice();
+        showScore();
+    }else if (humanChoice == 'rock' && computerChoice == 'paper'){
+        console.log("Computer wins!");
+        showChoice();
+        ++computerScore;
+        showScore();
+    }else if (humanChoice == 'rock' && computerChoice == 'scissor'){
+        console.log("You win!");
+        showChoice();
+        ++humanScore;
+        showScore();
+    }else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        console.log("You win!");
+        showChoice();
+        ++humanScore;
+        showScore();
+    }else if (humanChoice == 'paper' && computerChoice == 'scissor'){
+        console.log("Computer wins!");
+        showChoice();
+        ++computerScore;
+        showScore();
+    }else if (humanChoice == 'scissor' && computerChoice == 'rock'){
+        console.log("Computer wins!");
+        showChoice();
+        ++computerScore;
+        showScore();
+    }else {
+        console.log("You win!");
+        showChoice();
+        ++humanScore;
+        showScore();
+    }
+}
+
+playRound(humanSelection,computerSelection);
