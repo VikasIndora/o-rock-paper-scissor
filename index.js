fcce55e3aa79eb70e12fb704cf23bcf611965e35
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-07 17:35:39 +0530

    Add 2 core functions to get human/computer choices
    The getHumanChoice() takes a user input via prompt and getComputerChoice() generates a random choice for the computer

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello')
+function getHumanChoice(){
+    let userInput = prompt("Enter your pick (among rock, paper and scissor)").toLowerCase();
+    if (userInput === ''){
+        alert('The page will refresh and then enter an input')
+        return location.replace(location.href);
+    } else if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissor'){
+        return userInput;
+    } else{
+        alert('The page will refresh and then enter a valid input')
+        return location.replace(location.href);
+    }
+}
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0){
+        return "rock"
+    } else if (randomNumber === 1){
+        return "paper"
+    } else {
+        return "scissor"
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+console.log(humanSelection)
+console.log(computerSelection)
