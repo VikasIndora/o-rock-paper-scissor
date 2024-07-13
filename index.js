d7fc2c34a1d521059d7b1f81a44e67c17d38aae7
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-13 18:31:25 +0530

    Add div in memory and DOM to appear when 'clicked'
    The text needed to be displayed is happening but those are textNodes so they are in one line. which I don't want.

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getHumanChoice(){
-    let userInput = prompt("Enter your pick (rock, paper and scissor)").toLowerCase().trim();
-    if (userInput === '' || userInput === null){
-        alert('The page will refresh and then enter an input')
-        location.replace(location.href); // with no return value set any function returns 'undefined'.
-    } else if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissor'){
-        return userInput;
-    } else{
-        alert('The page will refresh and then enter a valid input')
-        location.replace(location.href);
-    }
-}
-
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){
@@ -26,44 +13,44 @@ function getComputerChoice(){
 }
 
 function showScore(){
-    console.log(`Your score is: ${humanScore}, Computer score is: ${computerScore}`);
+    displaySection.appendChild(document.createTextNode(`Your score is: ${humanScore}, Computer score is: ${computerScore}`))
 }
 function showChoice(){
-    console.log(`You chose: ${humanSelection}, Computer chose: ${computerSelection}`)
+    displaySection.appendChild(document.createTextNode(`You chose: ${humanSelection}, Computer chose: ${computerSelection}`))
 }
 
 function playRound(humanChoice,computerChoice){
     if (humanChoice == computerChoice){
-        console.log("Its a draw..");
+        displaySection.appendChild(document.createTextNode('Its a Draw..'));
         showChoice();
         showScore();
     }else if (humanChoice == 'rock' && computerChoice == 'paper'){
-        console.log("Computer wins!");
+        displaySection.appendChild(document.createTextNode("Computer wins!"));
         showChoice();
         ++computerScore;
         showScore();
     }else if (humanChoice == 'rock' && computerChoice == 'scissor'){
-        console.log("You win!");
+        displaySection.appendChild(document.createTextNode("You win!"));
         showChoice();
         ++humanScore;
         showScore();
     }else if (humanChoice == 'paper' && computerChoice == 'rock'){
-        console.log("You win!");
+        displaySection.appendChild(document.createTextNode("You win!"));
         showChoice();
         ++humanScore;
         showScore();
     }else if (humanChoice == 'paper' && computerChoice == 'scissor'){
-        console.log("Computer wins!");
+        displaySection.appendChild(document.createTextNode("Computer wins!"));
         showChoice();
         ++computerScore;
         showScore();
     }else if (humanChoice == 'scissor' && computerChoice == 'rock'){
-        console.log("Computer wins!");
+        displaySection.appendChild(document.createTextNode("Computer wins!"));
         showChoice();
         ++computerScore;
         showScore();
     }else {
-        console.log("You win!");
+        displaySection.appendChild(document.createTextNode("You win!"));
         showChoice();
         ++humanScore;
         showScore();
@@ -81,36 +68,23 @@ rock.addEventListener('click', (e)=>{
     humanSelection = 'rock';
     computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+    bodySection.appendChild(displaySection)
 })
 
 paper.addEventListener('click', (e)=>{
     humanSelection = 'paper';
     computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+    bodySection.appendChild(displaySection)
 })
 
 scissor.addEventListener('click', (e)=>{
     humanSelection = 'scissor';
     computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+    bodySection.appendChild(displaySection)
 })
 
-function playGame(){
-    for(let i = 1; i<=5; i++){
-        humanSelection = getHumanChoice();
-        if (humanSelection == undefined) {return;} // exit playGame() for an 'undefined' return value in getHumanChoice().
-        computerSelection = getComputerChoice();
-        playRound(humanSelection,computerSelection);
-        console.log("--------------------------------------")
-    }
-    console.log(`Final Scores:`) + console.log(showScore());
-    if (humanScore == computerScore){
-        console.log(`Jeez.. all that for a draw.`)
-    }else if (humanScore > computerScore){
-        console.log(`Hurray! You are THE ultimate champion!! (saviour of humanity)`)
-    }else {
-        console.log(`Its over.. in the end computer wins (end of humanity)`)
-    }
-}
-
-// playGame();
+const displaySection = document.createElement('div');
+displaySection.style.cssText = "border-radius: 2rem; border: 2px solid hotpink; padding: 4rem;"
+const bodySection = document.querySelector('body');
